d5a619722ea1073eb18d08c0bcac814939c6c1b1
rantar2 <82614995+rantar2@users.noreply.github.com>
2022-02-20 18:08:41 -0500

    added basic text field
    search bar and submit button added. when a username is entered it's logged in the browser console
    I have no idea how to move elements around lol

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,32 @@ class App extends Component {
     constructor(props) {
         super(props);
     }
-
     componentDidMount() {
         document.title = "MARS - My Anime Recommendations"
+    }
+
+    state = {
+      userName: ""
+    }
+    handleInput = event => {
+      this.setState({userName: event.target.value});
+    }
+    logUserName = () => {
+      console.log(this.state.userName);
     }
 
     render() {
         return (
             <div className="App">
                 <Header />
-                <div className="App-body">
-                    <p>
-                        We will add stuff here
-                    </p>
+                <div  className="App-body">
+                    <h2>
+                        Find Anime Recommendations
+                    </h2>
+                    <div className="Search-Bar">
+                      <input onChange={this.handleInput} placeholder="Enter MAL Username"/>
+                      <button onClick={this.logUserName}> Search </button>
+                    </div>
                 </div>
             </div>
         );
