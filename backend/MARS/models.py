b1d0f4a327881b51b4381aa5cfb0f12e7897d266
rantar2 <82614995+rantar2@users.noreply.github.com>
2022-03-17 19:03:38 -0400

    Django DB added
    Because MAL's API sucks, we have to maintain a backend database in order to have easy access to a master list of anime and their associated data. Currently, MAL's API doesn't allow search by genre, only keyword.
    
    I added an AnimeEntry class model that (currently) saves a show's name, rank, MAL ID, and a base64 encoded string of the list of genres. I did this for simplicity, although there may be better ways to do it.
    
    When the updateDB function is called, a given number of instances of this class are added to the backend database. Old data is currently being overwritten each time it's called.
    
    There hasn't been any frontend functionality added, but I think this is a step in that direction. What needs to be done now is checking the database against the user's MAL list to find recommendations, as well as a refactor (way too many free functions). I leave those as exercises for the reader.

--- backend/MARS/models.py
+++ backend/MARS/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import base64
+import json
 
 # Create your models here.
 class User(models.Model):
@@ -10,5 +12,13 @@ class User(models.Model):
 
 class Search(models.Model):
     userName = models.CharField(max_length=16)
+    def _str_(self):
+        return self.title
+
+class AnimeEntry(models.Model):
+    name = models.CharField(max_length=120)  # for long titled anime
+    animeID = models.IntegerField()  # Numerical ID corresponding to anime in MAL DB
+    rank = models.IntegerField()
+    genres = models.CharField(max_length=16)  # base64 encoded string of genres
     def _str_(self):
         return self.title
--- backend/MARS/recommender.py
+++ backend/MARS/recommender.py
@@ -1,7 +1,13 @@
 from urllib import request
+from .models import AnimeEntry
 import malclient
 import secrets
 import requests
+import base64
+import json
+import time
+
+# Notice: This should all be refactored into classes later
 
 clientID = "54082defd56574106879639dd3f91e47"
 clientSecret = "ab20781868791a1ab2999c963953048d20cfd353d9d3ba98a9b06717caa73657"
@@ -14,13 +20,92 @@ def getList(username):
     })
     response.raise_for_status()
     userList = response.json()
+    """
+    # Anime entries are formatted like a dictionary as follows:
+    "data":{
+        "node":{
+            "id":420,
+            "title":"watashi no anime",
+            "main_picture":{"medium":"https://...","large":"https://..."},
+            "genres":[{"id":69, "name":"slice of life"},...]
+            }
+        }
+
+    """
     response.close()
 
     return userList
 
+def updateDB(n):
+    """
+    Retrives n number of anime by ranking, then updates internal database. This
+    should run regularly, but not too frequently as to avoid hitting MAL's limit
+    (error handling to come later).
+    """
+    j = n//500  # Number of iterations
+    if n%500 != 0 and j > 0:
+        print(f"n should be a multiple of 500. Rounding to {j*500} entries.")
+    elif j==0:
+        print(f"n should be a multiple of 500. Rounding to 500 entries.")
+        j=1
+
+    """
+    Anime entries returned below are as follows:
+    "data":{
+        "node":{
+            "id":747,
+            "title":"boku no anime",
+            "main_picture":{"medium":"https://...","large":"https://..."},
+            "genres":[{"id":69, "name":"slice of life"},...],
+            }
+        "ranking":{
+            "rank":666
+            }
+        }
+    This Django DB toutorial may be helpful:
+    https://riptutorial.com/django/example/4015/basic-django-db-queries
+    """
+    for i in range(j):
+        # We can specify which types of anime to index by changing the url below.
+        # Currently, it retrives anime by popularity, but we can specify movies, TV series, etc
+        # We can also change the "fields" to include other data.
+        url = f"https://api.myanimelist.net/v2/anime/ranking?ranking_type=bypopularity&fields=genres&limit=500&offset={i*500}"
+        response = requests.get(url, headers= {
+            "X-MAL-CLIENT-ID": clientID
+        })
+        response.raise_for_status()
+        animeList = response.json()
+        for anime in animeList["data"]:
+            newEntry = AnimeEntry(
+                name=anime["node"]["title"],
+                rank=anime["ranking"]["rank"],
+                animeID=anime["node"]["id"],
+                genres=encode(anime["node"]["genres"])
+            )
+            newEntry.save()
+        response.close()
+
+def encode(genreList):
+    """
+    Base64 encodes a list of dictionaries of anime genres, ie.
+    INPUT:   [{"id":69, "name":"slice of life"},...]
+    OUTPUT: WzEsIDIsIDNd
+    """
+    return base64.b64encode(json.dumps(genreList).encode()).decode()
+
+def decode(base64EncodedList):
+    """
+    Decodes base64 into usable list of dictionaries.
+    INPUT: WzEsIDIsIDNd
+    OUTPUT: [{"id":69, "name":"slice of life"},...]
+    """
+    return json.loads(base64.b64decode(base64EncodedList.encode()).decode())
+
+
 def recommend(userList):
     genreDict = {}
     titleList = []
+    #print(userList["data"])
     for i in userList["data"]:
         titleList.append(i["node"]["title"])
         for genre in i["node"]["genres"]:
@@ -36,7 +121,11 @@ def recommend(userList):
     recString += "\nTop Shows: \n"
     for i in range(0, 10):
         recString += titleList[i] + "\n"
-    
 
+    AnimeEntry.objects.all().delete()  # Overwrite old entries. Maybe not the most efficient...
+    updateDB(1000)  #Adds/replaces first 1000 most popular shows on MAL.
+    for i in AnimeEntry.objects.all():  # Example: prints lists of anime and genres from DB
+        print(i.name, decode(i.genres))
+        pass
     print(recString)
     return recString
